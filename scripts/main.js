65249ad277e52304090c40d18ccf179692dbbc1b
Ann Kiende <41993451+annvel@users.noreply.github.com>
2023-03-23 17:40:18 +0300

    Modify main.js
    add playROund function and the logic with nested if...else loops

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,8 @@ function getComputerChoice() {
     rpsArrItem = rpsArr[rpsArrIndex];
     return rpsArrItem;
 }
-getComputerChoice()
-console.log(getComputerChoice());
+getComputerChoice();
+console.log("COMPUTER CHOICE: ", getComputerChoice());
 
 
 /*
@@ -18,8 +18,49 @@ function: playRound(playerSelection, computerSelection)
 plays a single round of the game
 returns, for example: "You Lose! Paper beats Rock"
 parameters are case-insensitive
+- rock beats scissors
+- paper beats rock
+- scissors beats paper
 */
-
+function playRound(playerSelection, computerSelection) {
+    console.log("USER CHOICE: Rock");
+    
+    if(typeof playerSelection !== "string") {
+        return "Enter something valid, for example, Rock, Paper or Scissors!" 
+    } else {
+        if(playerSelection === "rock") {
+            if(computerSelection == "Rock") {
+                return "Draw! Try again";
+            } else if(computerSelection == "Scissors") {
+                return "You Lose! Rock beats Scissors";
+            } else if(computerSelection == "Paper") {
+                return "You win! Paper beats Rock";
+            }
+        }  else if(playerSelection === "paper") {
+            if(computerSelection == "Paper") {
+                return "Draw! Try again";
+            } else if(computerSelection == "Scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else if(computerSelection == "Rock") {
+                return "You win! Paper beats Rock";
+            }
+        } else if(playerSelection === "Scissors") {
+            if(computerSelection == "Scissors") {
+                return "Draw! Try again";
+            }else if(computerSelection == "Rock") {
+                return "You Lose! Scissors beats Rock";
+            } else if(computerSelection == "Paper") {
+                return "You win! Scissors beats Paper";
+            }
+        }
+        /*else {
+            return `You win ${playerSelection} beats ${computerSelection}`;
+        }*/
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 /*
 return the results of function call NOT console.log
