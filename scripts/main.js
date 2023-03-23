4e5eb5d75b227dbdddb537b3fbfdeee09d0a012b
Ann Kiende <41993451+annvel@users.noreply.github.com>
2023-03-23 15:26:14 +0300

    Modify main.js
    lay out what is required per step and its gotten easier to understand where I am in this phase

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+/*
+function: getComputerChoice
+randomly returns a component of the game; ie, rock, paper or scissors
+*/
+
+
+/*
+function: playRound(playerSelection, computerSelection)
+plays a single round of the game
+returns, for example: "You Lose! Paper beats Rock"
+parameters are case-insensitive
+*/
+
+
+/*
+return the results of function call NOT console.log
+*/
+
+
+/*
+function: game
+call playRound function inside this function
+Intention: play 5 round game that:
+- keeps the score
+- reports a winner or loser at the end
+Recommendation: Use loops, ie, for loops
+Use console.log to display results of each round & winner at the end
+Use prompt to get user's input
+*/
